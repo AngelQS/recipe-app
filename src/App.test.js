9ae35ad66c2b54c28d87e995c01f8ff1d3fb059d
Giraficode <giraficode@gmail.com>
2021-10-17 04:02:23 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// @ponicode
+describe("componentDidMount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", async () => {
+        await inst.componentDidMount()
+    })
+})
+
+// @ponicode
+describe("componentDidUpdate", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidUpdate()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
